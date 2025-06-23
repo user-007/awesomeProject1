742b3d28bf955d1afe36d73c7645ba27040637f8
Yoan <yoan.ivanov0007@gmail.com>
2025-06-23 07:07:01 +0300

    added .toml config, Get route

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, Worlds!")
 	//var myName string = "Johnm Doe"
 	//const mySecondName string = "Jane Doe"
 	//
@@ -15,5 +15,17 @@ func main() {
 	//fmt.Println(myName)
 	//fmt.Println(mySecondName)
 	app := fiber.New()
+	//add route
+	type Todo struct {
+		ID        int    `json:"id"`
+		Completed bool   `json:"completed"`
+		Body      string `json:"body"`
+	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{
+			"msg": "Hello World"})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
