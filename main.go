7a7f6b1361a374834d53b948ab12b3252e6b9ee2
Yoan <yoan.ivanov0007@gmail.com>
2025-06-23 07:26:06 +0300

    added Post route, todo

--- main.go
+++ main.go
@@ -22,10 +22,34 @@ func main() {
 		Body      string `json:"body"`
 	}
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{
 			"msg": "Hello World"})
 	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} // 0 completed empty
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body..."})
+		}
+		//increment
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+	var x int = 5
+	var p *int = &x //
+
+	fmt.Println(p)
+	fmt.Println(*p)
 
 	log.Fatal(app.Listen(":4000"))
 }
